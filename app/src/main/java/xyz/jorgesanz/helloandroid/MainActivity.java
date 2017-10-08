1ff1c931dd239e69201a4969b51572b6fb180ca4
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-10-08 20:08:55 +0200

    feat: Create all required activities

---
+++ app/src/main/java/xyz/jorgesanz/helloandroid/MainActivity.java
@@ -0,0 +1,21 @@
+package xyz.jorgesanz.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameInput;
+    private Button helloButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameInput = (EditText) findViewById(R.id.name_input);
+        helloButton = (Button) findViewById(R.id.hello_button);
+    }
+}
