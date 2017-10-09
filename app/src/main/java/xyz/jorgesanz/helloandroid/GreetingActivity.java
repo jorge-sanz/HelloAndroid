b8df1cf7e9f590ad58d4f9489322bd18181daeb6
Jorge Sanz Pérez <jorsanzp@gmail.com>
2017-10-09 19:56:19 +0200

    feat: Say hello to user

--- app/src/main/java/xyz/jorgesanz/helloandroid/GreetingActivity.java
+++ app/src/main/java/xyz/jorgesanz/helloandroid/GreetingActivity.java
@@ -2,12 +2,21 @@ package xyz.jorgesanz.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GreetingActivity extends AppCompatActivity {
+
+    private TextView greetingText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_greeting);
+
+        greetingText = (TextView) findViewById(R.id.greeting_label);
+
+        Bundle bundle = this.getIntent().getExtras();
+
+        greetingText.setText("Hello, " + bundle.getString("NAME"));
     }
 }
--- app/src/main/java/xyz/jorgesanz/helloandroid/MainActivity.java
+++ app/src/main/java/xyz/jorgesanz/helloandroid/MainActivity.java
@@ -1,14 +1,14 @@
 package xyz.jorgesanz.helloandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText nameInput;
-    private Button helloButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nameInput = (EditText) findViewById(R.id.name_input);
-        helloButton = (Button) findViewById(R.id.hello_button);
+    }
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(MainActivity.this, GreetingActivity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("NAME", nameInput.getText().toString());
+
+        intent.putExtras(bundle);
+
+        startActivity(intent);
     }
 }
